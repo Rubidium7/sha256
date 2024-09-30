2e061fbb7396b47338106e7d3302e6706371c0c8
Noel Lonka <nlonka@c1r6p21.hive.fi>
2024-09-30 16:10:24 +0300

    feat(core): add base for the algorithm and input taking

---
+++ src/main.rs
@@ -0,0 +1,180 @@
+use std::env;
+use std::error;
+use std::error::Error;
+use std::fs;
+use std::fmt;
+use std::io;
+use std::io::Read;
+use std::io::BufReader;
+use std::num::Wrapping;
+
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+#[derive(Debug, Clone)]
+struct InputError;
+
+impl Error for InputError{}
+
+impl fmt::Display for InputError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "usage: ./sha256 (= reads stdin)\nor\nusage: ./sha256 -s <string>\nor\nusage: ./sha256 <filename>")
+	}
+}
+
+const h0: u32 = 0x6a09e667;
+const h1: u32 = 0xbb67ae85;
+const h2: u32 = 0x3c6ef372;
+const h3: u32 = 0xa54ff53a;
+const h4: u32 = 0x510e527f;
+const h5: u32 = 0x9b05688c;
+const h6: u32 = 0x1f83d9ab;
+const h7: u32 = 0x5be0cd19;
+
+const K: [u32; 64] = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2];
+
+
+fn rotate_left (x: u32, n: u32) -> u32 {
+	(x << n) | (x >> (32 - n))
+}
+
+fn rotate_right (x: u32, n: u32) -> u32 {
+	(x >> n) | (x << (32 - n))
+}
+
+// fn print_bytes(s: &str) {
+// 	let bytes = s.as_bytes();
+
+// 	for byte in bytes.iter() {
+// 		println!("{byte:x}");
+// 	}
+// }
+
+// fn print_bytes(s: &Vec<u8>) {
+
+// 	for byte in s.iter() {
+// 		print!("{byte:x} ");
+// 	}
+// 	println!()
+// }
+
+fn print_hash(hash: [u32; 4]) {
+	
+    let string = format!(
+        "{:08x}{:08x}{:08x}{:08x}",
+        hash[0].swap_bytes(),
+        hash[1].swap_bytes(),
+        hash[2].swap_bytes(),
+        hash[3].swap_bytes()
+    );
+
+	println!("{string}");
+}
+
+fn make_padded_vec(input: &str) -> Vec<u8> {
+
+	let og_len_in_bits = Wrapping(input.len() as u64 * 8);
+	let mut input_vec: Vec<u8> = Vec::new();
+	input_vec.extend(input.as_bytes());
+	input_vec.push(128_u8);
+
+	while (input_vec.len() * 8) % 512 != 448 {
+		input_vec.push(0_u8);
+	}
+	let og_len_in_bytes : [u8; 8] = og_len_in_bits.0.to_le_bytes();
+	let mut og_len_in_bytes = og_len_in_bytes.to_vec();
+	input_vec.append(&mut og_len_in_bytes);
+
+	input_vec
+}
+
+fn transform_to_u32(input: &Vec<u8>, start: usize) -> Vec<u32>  {
+	let mut words: Vec<u32> = Vec::new();
+
+	let mut	tmp: Vec<u8> = Vec::new(); 
+
+	let len: usize = start + 64;
+
+	let mut byte_count = 0;
+	for i in start..len {
+		tmp.push(input[i].clone());
+		if byte_count == 3 {
+            let byte_arr: [u8; 4] = tmp.clone().try_into().unwrap();
+			let word: u32 = u32::from_ne_bytes(byte_arr);
+			words.push(word);
+			tmp.clear();
+			byte_count = 0;
+		}
+		else {
+			byte_count += 1;
+		}
+	}
+	words
+}
+
+fn hash_loop(input: Vec<u8>, hash: &mut [u32; 4]) {
+	
+}
+
+fn read_from_stdin() -> Result<String>
+{
+	let mut result = String::new();
+
+	for line in io::stdin().lines() {
+		let tmp = line?;
+		result = result.clone() + &tmp;
+	}
+	Ok(result)
+}
+
+fn get_input(args: &Vec<String>) -> Result<String> {
+	match args.len() {
+		1 => read_from_stdin(),
+		2 => {
+			if args[1] == "-s" {				
+				return Err(Box::new(InputError));
+			}
+			let infile = fs::File::open(args[1].clone())?;
+			
+			let mut buf_reader = BufReader::new(infile);
+			let mut input = String::new();
+			let result = buf_reader.read_to_string(&mut input);
+			match result {
+				Ok(_) => Ok(input),
+				Err(error) => Err(Box::new(error)),
+			}
+		},
+		3 => {
+			if args[1] != "-s" {
+				return Err(Box::new(InputError));
+			}
+			Ok(String::from(args[2].clone()))
+		},
+		_ => { 
+			Err(Box::new(InputError))			
+		},
+	}
+}
+fn main() -> Result<()> {
+	let args: Vec<String> = env::args().collect();
+	
+	let input = get_input(&args).unwrap_or_else(|error| {
+		eprintln!("{error}");
+		panic!("{error:?}");
+	});
+	
+	let mut hash: [u32; 4] = [A, B, C, D];
+	
+	let padded: Vec<u8> = make_padded_vec(&input);
+	
+	hash_loop(padded, &mut hash);
+	
+	print_hash(hash);
+	Ok(())
+}
